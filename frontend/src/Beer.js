e1d19e069c6746728d915916ba88158412ff5283
David Albalate Cabedo <david@netshell.co.uk>
2018-09-28 09:19:50 +0100

    Added url paarams in a call

--- frontend/src/Beer.js
+++ frontend/src/Beer.js
@@ -4,12 +4,18 @@ import { Route,Link } from "react-router-dom";
 function Beer(props){
     return (
         <li>
-            <Link to={/beer/`${props.id}`}>{props.name}</Link>
-            <Route path={/beer/`${props.id}`} render= {({match}) =>( <div> <h3> {match.params.name} </h3></div>)}/>
+            <Link to={`/beer/${props.id}`}>{props.name}</Link>
+            <Route path={`/beer/${props.id}`} component={BeerDetails}/>
         </li>
 
     );
 
 }
+
+const BeerDetails = ({ match }) => (
+    <div>
+        <h3>ID: {match.params.id}</h3>
+    </div>
+);
 
 export default Beer;
